e9ddf79f6eb1147ca585801b30de7d4445596bdc
johnnymutisya <johnnymutisya@gmail.com>
2018-11-07 14:50:01 +0300

    Updated at Wed Nov  7 14:50:01 EAT 2018

--- app/src/main/java/com/digischool/digischool/EnrollmentActivity.java
+++ app/src/main/java/com/digischool/digischool/EnrollmentActivity.java
@@ -1,13 +1,17 @@
 package com.digischool.digischool;
 
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences.Editor;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -200,6 +204,26 @@ public class EnrollmentActivity extends AppCompatActivity {
         cursor.close();
 
         return path;
+    }
+
+    String TAG="STORAGE_PERM";
+    public  boolean isStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.v(TAG,"Permission is granted");
+                return true;
+            } else {
+
+                Log.v(TAG,"Permission is revoked");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.v(TAG,"Permission is granted");
+            return true;
+        }
     }
 
 }
