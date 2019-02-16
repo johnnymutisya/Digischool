f8b68ff27b0c0f2fab1b0c8a301a80b29e70d62f
johnnymutisya <johnnymutisya@gmail.com>
2019-02-16 13:20:01 +0300

    Updated at Sat Feb 16 13:20:01 EAT 2019

--- app/src/main/java/com/digischool/digischool/reports/AttendanceReports.java
+++ app/src/main/java/com/digischool/digischool/reports/AttendanceReports.java
@@ -7,6 +7,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -108,6 +109,7 @@ public class AttendanceReports extends AppCompatActivity {
             @Override
             public void onSuccess(int statusCode, Header[] headers, String responseString) {
                 progress.dismiss();
+                Log.d("SERVER_SAYS", "onSuccess: "+responseString);
                 try
                 {
                     JSONObject obj=new JSONObject(responseString);
