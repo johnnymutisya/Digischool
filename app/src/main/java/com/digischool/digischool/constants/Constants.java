fa0c1b1b28acfbd04ed4b5cfa43f38c7d1ca0bef
Erik Dubois <erik.dubois@gmail.com>
2019-08-28 16:20:01 +0300

    Updated at Wed Aug 28 16:20:01 EAT 2019

--- app/src/main/java/com/digischool/digischool/constants/Constants.java
+++ app/src/main/java/com/digischool/digischool/constants/Constants.java
@@ -2,4 +2,5 @@ package com.digischool.digischool.constants;
 
 public class Constants {
     public static String BASE_URL = "http://66.228.55.80:8000/digisch/";
+   // public static String BASE_URL = "http://66.228.55.80:8000/health/";
 }
