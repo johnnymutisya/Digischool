1170f9528d336423053354e52f072deec8811188
Erik Dubois <erik.dubois@gmail.com>
2019-11-05 16:00:01 +0300

    Updated at Tue Nov  5 16:00:01 EAT 2019

--- app/src/main/java/com/digischool/digischool/attendance/NTS_Attendance_Reports.java
+++ app/src/main/java/com/digischool/digischool/attendance/NTS_Attendance_Reports.java
@@ -2,17 +2,18 @@ package com.digischool.digischool.attendance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 
 import com.digischool.digischool.R;
 
 public class NTS_Attendance_Reports extends AppCompatActivity {
-   ListView listView;
+   ExpandableListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nts__attendance__reports);
-        listView=findViewById(R.id.nts_attendance_list);
+        listView=findViewById(R.id.listAttendanceExpandable);
 
 
     }
