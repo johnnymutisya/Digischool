057084eb704d4f8ba7f9e4b63606b19a109959c3
Erik Dubois <erik.dubois@gmail.com>
2019-11-05 11:40:01 +0300

    Updated at Tue Nov  5 11:40:01 EAT 2019

---
+++ app/src/main/java/com/digischool/digischool/Teachers_Attendance_Reports.java
@@ -0,0 +1,13 @@
+package com.digischool.digischool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Teachers_Attendance_Reports extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher__attendance);
+    }
+}
