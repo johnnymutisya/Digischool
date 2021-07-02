085006b410d491f9ff2ed652c976295a4dfa7e84
Erik Dubois <erik.dubois@gmail.com>
2021-07-02 17:00:01 +0300

    Updated at Fri Jul  2 17:00:01 EAT 2021

--- app/src/main/java/com/digischool/digischool/constants/Constants.java
+++ app/src/main/java/com/digischool/digischool/constants/Constants.java
@@ -1,6 +1,6 @@
 package com.digischool.digischool.constants;
 
 public class Constants {
-    public static String BASE_URL = "http://66.228.55.80:8000/digisch/";
-    public static String TEMPLATE_URL = "http://66.228.55.80:8800/api/";
+    public static String BASE_URL = "http://194.233.166.164:8000/digisch/";
+    public static String TEMPLATE_URL = "http://194.233.166.164:8800/api/";
 }
